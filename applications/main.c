634e245ba925abfd1914a943f0336f058e9966f3
luoqi <logeexpluoqi@outlook.com>
2023-07-02 01:54:24 +0800

    first add files

---
+++ applications/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-07-02     RT-Thread    first version
+ */
+
+#include <rtthread.h>
+
+int main(void)
+{
+
+    return RT_EOK;
+}
