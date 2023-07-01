0d6b6e75b41988c2424c7af6b15e39d66e2bdefd
luoqi <logeexpluoqi@outlook.com>
2023-07-02 02:21:55 +0800

    code(shell): delay 1s to start msh

--- rt-thread/components/finsh/shell.c
+++ rt-thread/components/finsh/shell.c
@@ -768,7 +768,7 @@ int finsh_system_init(void)
     finsh_system_function_init(ptr_begin, ptr_end);
 #endif
 #endif
-
+    rt_thread_mdelay(1000);
 #ifdef RT_USING_HEAP
     /* create or set shell structure */
     shell = (struct finsh_shell *)rt_calloc(1, sizeof(struct finsh_shell));
